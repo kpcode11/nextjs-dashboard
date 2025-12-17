3802c9898f966e90c4b1a1a08e7b67b7ee5bc950
kpcode11 <prajapatikeshav497@gmail.com>
2025-12-17 23:13:49 +0530

    fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,6 +6,7 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData,} from "@/app/lib/data
 
 export const dynamic = 'force-dynamic';
 
+
 export default async function Page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
